4561321cf918f39fdf229e61d88b8bce13594526
Atharva Mandle <atharvamandle19@gmail.com>
2025-12-18 23:23:14 +0530

    cart Serice made

--- src/main/java/com/ecommerce/service/CartService.java
+++ src/main/java/com/ecommerce/service/CartService.java
@@ -1,5 +1,24 @@
 package com.ecommerce.service;
 
+import com.ecommerce.model.Cart;
+
 public interface CartService {
+
+    Cart getCartByUser(Long userId);
+
+    double getTotal(Long cartId);
+
+    String clearCart(Long cartId);
+
+    Cart cartInfo(Long userId);
+
+    Cart addProductToCart(Long userId, Long productId, int quantity);
+
+    Cart updateProductQuantity(Long userId, Long productId, int quantity);
+
+    Cart removeProductFromCart(Long userId, Long productId);
+
+
+
 
 }
