60ce867cebc9648b3de674af50a10a20b3c60aa8
Atharva Mandle <atharvamandle19@gmail.com>
2025-12-20 11:09:16 +0530

    Order Service

---
+++ src/main/java/com/ecommerce/service/OrderService.java
@@ -0,0 +1,18 @@
+package com.ecommerce.service;
+
+import com.ecommerce.model.Cart;
+import com.ecommerce.model.Order;
+
+import java.util.List;
+
+public interface OrderService {
+
+    Order placeOrder(Long userId);
+
+    List<Order> orderHistory(Long userId);
+
+    Order orderDetails(Long orderId);
+
+    void cancelOrder(Long orderId);
+
+}
